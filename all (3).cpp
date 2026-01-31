2bdc63f1450432448b99e9081fec68610224e77a
Shubhodeep Supakar <supakar121@gmail.com>
2026-01-31 23:04:19 +0530

    Add files via upload
    A. Be Positive CodefForces 800 Rating questions

--- all (3).cpp
+++ all (3).cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     int t;
     cin >> t;
-    while(t--) {
-        string a;
-        cin >> a;
-        
-        reverse(a.begin(), a.end());
-
-        for(char &c : a) {
-            if(c == 'p') c = 'q';
-            else if(c == 'q') c = 'p';
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        int neg = 0, zero = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            if (a[i] == -1) neg++;
+            if (a[i] == 0) zero++;
         }
-
-        cout << a << endl;
+        int operations = 0;
+        operations += zero;
+        if (neg % 2 == 0) {
+            cout << operations << "\n";
+        } else {
+            operations += 2;
+            cout << operations << "\n";
+        }
     }
     return 0;
 }
