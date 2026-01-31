0daca5e5bece42686b45c53c47405fb3902a5ac4
Shubhodeep Supakar <supakar121@gmail.com>
2026-01-31 22:58:43 +0530

    Add files via upload
    2051A - Preparing for the Olympiad - 800 rating Codeforces

--- all (2).cpp
+++ all (2).cpp
@@ -1,23 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
-    
-    while(t--) {
-        int x;
-        cin >> x;
-        int ans;
-
-        //int ans = 9;
-        while(x > 0) {
-            int d = x % 10;
-            ans = min(ans, d);
-            x /= 10;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; i++) 
+        cin >> a[i];
+        for (int i = 0; i < n; i++) 
+        cin >> b[i];
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int gain = a[i] - b[i + 1];
+            if (gain > 0) 
+            ans += gain;
         }
-
-        cout << ans << endl;
+        ans += a[n - 1];
+        cout << ans << "\n";
     }
     return 0;
 }
