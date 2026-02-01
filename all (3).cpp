4f935539d45ccd676678627701d40b96508c7403
Shubhodeep Supakar <supakar121@gmail.com>
2026-02-01 22:18:44 +0530

    Add files via upload
    1866A. Ambitious Kid CodeForces 800 Rating Problem!

--- all (3).cpp
+++ all (3).cpp
@@ -1,27 +1,20 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
 using namespace std;
- 
+
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        int neg = 0, zero = 0;
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-            if (a[i] == -1) neg++;
-            if (a[i] == 0) zero++;
-        }
-        int operations = 0;
-        operations += zero;
-        if (neg % 2 == 0) {
-            cout << operations << "\n";
-        } else {
-            operations += 2;
-            cout << operations << "\n";
-        }
+    int n;
+    cin >> n;
+
+    long long x;
+    long long ans = LLONG_MAX;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (abs(x) < ans)
+            ans = abs(x);
     }
+
+    cout << ans;
     return 0;
 }
