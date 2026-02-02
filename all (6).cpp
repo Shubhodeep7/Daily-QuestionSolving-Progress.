175570691c1250cb9f99f9766c7d028eabd03d1c
Shubhodeep Supakar <supakar121@gmail.com>
2026-02-02 22:34:49 +0530

    Add files via upload
    2133B. Villagers CodeForces 800 Rating!

--- all (6).cpp
+++ all (6).cpp
@@ -1,22 +1,21 @@
-class Solution {
-public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size();
-        int low = 0, high = n - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            
-            int missing = arr[mid] - (mid + 1);
-
-            if (missing < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<long long> g(n);
+        for(int i = 0; i < n; i++)
+            cin >> g[i];
+        sort(g.begin(), g.end());
+        long long ans = 0;
+        for(int i = 1; i < n; i++) {
+            ans += g[i];   // sum all except smallest
         }
-
-        return low + k;
+        cout << ans << "\n";
     }
-};
+    return 0;
+}
+
