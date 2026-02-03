6cc4c7f6c8f00ed2f42bd065eeed3d2000140704
Shubhodeep Supakar <supakar121@gmail.com>
2026-02-03 18:22:21 +0530

    Add files via upload
    1614. Maximum Nested Depth of the parentheses. LeetCode Easy Level!

--- all (7).cpp
+++ all (7).cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-    int daysRequired(vector<int>& weights, int capacity) {
-        int days = 1, load = 0;
-
-        for (int w : weights) {
-            if (load + w > capacity) {
-                days++;
-                load = w;
-            } else {
-                load += w;
-            }
-        }
-        return days;
+    int maxDepth(string s) {
+    int a = 0;
+    int b = 0;
+    for(char c : s){
+        if(c == '(')
+        a = max(a,++b);
+        if(c == ')')
+        b--;
     }
-
-    int shipWithinDays(vector<int>& weights, int d) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int requiredDays = daysRequired(weights, mid);
-
-            if (requiredDays <= d)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return low;
+    return a;
     }
 };
