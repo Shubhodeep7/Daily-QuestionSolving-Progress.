d47214e341b63c6196e0c49e53a6cb559608d3b3
Shubhodeep Supakar <supakar121@gmail.com>
2026-02-07 17:05:12 +0530

    Add files via upload
    	1805A - We Need the Zero CodeForces 800 Rating!

---
+++ all (22).cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int xr = 0;
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin >> a;
+            xr ^= a;
+        }
+        if (n % 2 == 0) {
+            if (xr == 0) cout << 0 << "\n";
+            else cout << -1 << "\n";
+        } else {
+            cout << xr << "\n";
+        }
+    }
+    return 0;
+}
+
