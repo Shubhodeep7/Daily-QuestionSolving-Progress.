e7074752404dc1e96270a0518eeb4e4238184172
Shubhodeep Supakar <supakar121@gmail.com>
2026-02-08 22:55:33 +0530

    Add files via upload
    	1761A - Two Permutations 800 Rating CodeForces

---
+++ all (25).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if ((a == n && b == n) || (a + b <= n))
+            cout << "Yes\n";
+        else
+            cout << "No\n";
+    }
+    return 0;
+}
