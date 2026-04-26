f1c2abc6f3a5537de083d6d40af4ed93abac7d97
Shubhodeep Supakar <supakar121@gmail.com>
2026-04-26 23:20:40 +0530

    Add files via upload
    216. Combination Sum III Medium Level LeetCode

---
+++ dailyprogress/Daily-QuestionSolving-Progress.-main/Daily-QuestionSolving-Progress.-main/Questions Practice Series/216. Combination Sum III.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void backtrack(int start, int k, int n,
+                   vector<int>& temp,
+                   vector<vector<int>>& ans) {
+        if (temp.size() == k) {
+            if (n == 0)
+                ans.push_back(temp);
+            return;
+        }
+        for (int i = start; i <= 9; i++) {
+            if (i > n) break;
+            temp.push_back(i);
+            backtrack(i + 1, k, n - i, temp, ans);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        backtrack(1, k, n, temp, ans);
+        return ans;
+    }
+};
